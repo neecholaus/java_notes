c01c7d2d6d194449023b63785f75447b67d9b741
Nick Neuman <neecholaus@gmail.com>
2017-10-25 23:17:58 -0700

    JavaNotes-3 Put user parameter initialization into the constructor and handle first user option in main method.

--- note.java
+++ note.java
@@ -1,35 +1,60 @@
 import java.util.ArrayList;
 
 class note {
-    
 
-    public note() {
+    ArrayList<String> params = new ArrayList<String>();
 
+    public note(String[] arguments) {
+	
+	// Store user options
+	for(String param: arguments) {
+	    params.add(param);
+	}
+	
     }
 
-    public void add(String title, String content) {
 
+    
+    public void add(String title, String content) {
+	System.out.println("Adding note:");
     }
 
     public void remove(String title) {
-	
+	System.out.println("Removing note:");
     }
 
     public void list() {
-	
+	System.out.println("Listing all notes:");
     }
 
     public void view(String title) {
-	
+	System.out.println("Vewing note:");
     }
     
 
-    public static void main(String[] args) {
-	ArrayList<String> params = new ArrayList<String>();
-	for(String param: args) {
-	    params.add(param);
+    public static void main(String[] args) {	
+
+	note noteCall = new note(args);
+
+	switch (noteCall.params.get(0)) {
+	case "add":
+	    noteCall.add("title", "content");
+	    break;
+	case "remove":
+	    noteCall.remove("title");
+	    break;
+	case "list":
+	    noteCall.list();
+	    break;
+	case "view":
+	    noteCall.view("title");
+	    break;
+	default:
+	    noteCall.list();
+	    break;
 	}
-	System.out.println(params);
+	
+	
     }
     
 }
