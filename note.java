6de248e5101ab0a3312fcd5c525590b99509b56d
Nick Neuman <neecholaus@gmail.com>
2017-10-25 21:34:00 -0700

    JavaNotes-2 Initialized methods for different actions, stored user params as an ArrayList.

--- note.java
+++ note.java
@@ -1,10 +1,35 @@
-class note {
+import java.util.ArrayList;
 
+class note {
     
+
+    public note() {
+
+    }
+
+    public void add(String title, String content) {
+
+    }
+
+    public void remove(String title) {
+	
+    }
+
+    public void list() {
+	
+    }
+
+    public void view(String title) {
+	
+    }
     
 
     public static void main(String[] args) {
-	System.out.println("hi");
+	ArrayList<String> params = new ArrayList<String>();
+	for(String param: args) {
+	    params.add(param);
+	}
+	System.out.println(params);
     }
     
 }
